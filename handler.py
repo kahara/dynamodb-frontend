31f3b22c5767dd7f315b72294a3058af131ce5f2
Joni Kahara <joni.kahara@async.fi>
2013-03-23 18:14:30 +0200

    use Resource, HTTPResponse

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 from multiprocessing import Queue, current_process
 from request import HTTPRequest
-#form resource import UserResource
+from resource import UserResource
 
 def handler(is_running, request_q, response_q):
     
@@ -8,9 +8,9 @@ def handler(is_running, request_q, response_q):
     body += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
     body += b'Hello, world!'
     
-    # resource_map = {
-    #     'user': UserResource,
-    #     }
+    resource_map = {
+        'user': UserResource,
+        }
     
     while True and is_running.value:
         try:
@@ -23,10 +23,14 @@ def handler(is_running, request_q, response_q):
             # process incoming request and generate response
             http_request = HTTPRequest(request['raw'])
             
-            response = { 'id': request['id'], 'raw': body }
+            resource = resource_map[http_request.path[0]](http_request)
+            
+            response = resource.response.raw()
+            
+            print response
             
             # send response
-            response_q.put(response)
+            response_q.put({ 'id': request['id'], 'raw': response })
             
         except KeyboardInterrupt:
             pass
