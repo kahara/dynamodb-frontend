41c1537fcce9aae85609ada165bf2dd670059f2b
Joni Kahara <joni.kahara@async.fi>
2013-03-23 22:33:15 +0200

    create per pid DynamoDB connection, and per connection table of DynamoDB tables

--- resource.py
+++ resource.py
@@ -1,11 +1,27 @@
+from multiprocessing import current_process
 from response import HTTPResponse
+import boto.dynamodb
 
 class Resource(object):
+    connections = {}
+    
     def __init__(self, request):
         self.request = request
         self.response = None
+
+        self.pid = current_process().pid
+        if not self.pid in self.connections:
+            self.connections[self.pid] = {
+                'connection': boto.dynamodb.connect_to_region('eu-west-1'),
+                'tables': {}
+                }
+            
         self.handle()
-        
+            
 class UserResource(Resource):
     def handle(self):
+        if not 'user' in self.connections[self.pid]['tables']:
+            print 'get_table', self.pid
+            self.connections[self.pid]['tables']['user'] = self.connections[self.pid]['connection'].get_table('reader-dev-user')
+        
         self.response = HTTPResponse(status=200, headers={'foo': 'bar', 'baz': 'quux'}, body='i am the walrus')
