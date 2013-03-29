591b460d89b281c2915bb4d872cef4a98a985ae8
Joni Kahara <joni.kahara@async.fi>
2013-03-29 15:02:51 +0200

    handle request parsing failures

--- request.py
+++ request.py
@@ -7,19 +7,25 @@ class Request(object):
         self.headers = {}
         self.session_id = None
         self.body = None
+        self.is_valid = False
         
         self.parse()
 
     def parse(self):
-        request_line, headers = self.raw.split('\r\n', 1)
-        self.method, self.path, self.version = request_line.split(' ', 2)
-        self.path = [x for x in self.path.split('/') if x]        
-        headers, self.body = headers.split('\r\n\r\n', 1)
-        for header in headers.split('\r\n'):
-            key, value = header.split(':', 1)
-            self.headers[key.strip()] = value.strip()
-        
-        if 'Cookie' in self.headers:
-            key, value = self.headers['Cookie'].split(';')[0].split('=')
-            if key == 'session_id' and value:
-                self.session_id = value
+        try:
+            request_line, headers = self.raw.split('\r\n', 1)
+            self.method, self.path, self.version = request_line.split(' ', 2)
+            self.path = [x for x in self.path.split('/') if x]        
+            headers, self.body = headers.split('\r\n\r\n', 1)
+            for header in headers.split('\r\n'):
+                key, value = header.split(':', 1)
+                self.headers[key.strip()] = value.strip()
+
+            if 'Cookie' in self.headers:
+                key, value = self.headers['Cookie'].split(';')[0].split('=')
+                if key == 'session_id' and value:
+                    self.session_id = value
+            
+            self.is_valid = True
+        except:
+            self.is_valid = False
