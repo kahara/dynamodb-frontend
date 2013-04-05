cae6c33bf76d73beb8677de9755391a6083c83fd
Joni Kahara <joni.kahara@gmail.com>
2013-04-05 15:48:37 +0000

    "complete" user resource tests

--- tests/user_resource.py
+++ tests/user_resource.py
@@ -46,20 +46,37 @@ class TestUserResource(unittest.TestCase):
         resource = UserResource(request)
         
         activation_item = list(self.connection.get_table('activation').scan(scan_filter={'email': EQ(self.email)}))[0]
+
+        self.assertEqual(activation_item['email'], self.email)
+        self.assertEqual(activation_item['username'], self.username)
+        
         activation_id = activation_item['id']
         request = Request('POST /user/activation/%s HTTP/1.0\r\nAccept: application/json\r\n\r\n' % (activation_id, ))
         resource = UserResource(request)
+        
+        username_item = self.connection.get_table('username').get_item(hash_key=self.username)
+        user_item = self.connection.get_table('user').get_item(hash_key=username_item['user'])
+        self.assertEqual(user_item['email'], self.email)
+        self.assertEqual(user_item['username'], self.username)
+        self.assertTrue(self.password, user_item['password'])
         
         # test password reset
         body = json.dumps({'email': self.email})
         request = Request('POST /user/reset/ HTTP/1.0\r\nAccept: application/json\r\n\r\n%s' % (body, ))
         resource = UserResource(request)
         
+        self.password = self.password + '1234'
+        
         body = json.dumps({'password': self.password})
         reset_item = list(self.connection.get_table('reset').scan(scan_filter={'email': EQ(self.email)}))[0]
         reset_id = reset_item['id']
         request = Request('POST /user/reset/%s HTTP/1.0\r\nAccept: application/json\r\n\r\n%s' % (reset_id, body))
         resource = UserResource(request)
+        
+        user_item = self.connection.get_table('user').get_item(hash_key=username_item['user'])
+        self.assertEqual(user_item['email'], self.email)
+        self.assertEqual(user_item['username'], self.username)
+        self.assertTrue(self.password, user_item['password'])
         
 if __name__ == '__main__':
     unittest.main()
