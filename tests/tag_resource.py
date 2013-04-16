f35cd8c5373ad2c372236141f35fff739e5edddd
Joni Kahara <joni.kahara@gmail.com>
2013-04-16 16:45:19 +0000

    common test setUp, tearDown ops in base class

--- tests/tag_resource.py
+++ tests/tag_resource.py
@@ -1,89 +1,29 @@
 #!/usr/bin/env python                                                                                                                                                   
-
 import unittest, json, sys, boto.dynamodb
+from testbase import TestBase
 sys.path.insert(0, '..')
-from auth import generate_key, hash_password
+from auth import generate_key
 from request import Request
 from tag_resource import TagResource
-from session_resource import SessionResource
 
-class TestTagResource(unittest.TestCase):
-    def setUp(self):
-        self.userid = generate_key()
-        self.username = 'thisisatestuser'
-        self.email = 'thisisatestemail@example.com'
-        self.password = 'thisisatestpassword'
-        self.session_id = None
-        
-        connection =  boto.dynamodb.connect_to_region('eu-west-1')
-        
-        try:
-            username_item = connection.get_table('username').get_item(hash_key=self.username)
-            connection.get_table('user').get_item(hash_key=username_item['user']).delete()
-            username_item.delete()
-            # XXX scan test user tags and delete them
-        except: pass
-        try: connection.get_table('email').get_item(hash_key=self.email).delete()
-        except: pass
-        
-        connection.get_table('user').new_item(
-            hash_key=self.userid,
-            attrs={
-                'username': self.username,
-                'email': self.email,
-                'password': hash_password(self.password),
-                }
-            ).put()
-        
-        connection.get_table('username').new_item(
-            hash_key=self.username,
-            attrs={
-                'user': self.userid,
-                }
-            ).put()
-        
-        connection.get_table('email').new_item(
-            hash_key=self.email,
-            attrs={
-                'user': self.userid,
-                }
-            ).put()
-        
-        body = json.dumps({'login': self.username, 'password': self.password})
-        request = Request('POST /session/ HTTP/1.0\r\nAccept: application/json\r\n\r\n%s' % (body, ))
-        resource = SessionResource(request)
-        self.session_id = resource.response.headers['Set-Cookie'].split(' ')[0]
-        
-    def tearDown(self):
-        request = Request('DELETE /session/ HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.session_id, ))
-        resource = TagResource(request)
-        
-        connection =  boto.dynamodb.connect_to_region('eu-west-1')        
-        connection.get_table('user').get_item(hash_key=self.userid).delete()
-        connection.get_table('username').get_item(hash_key=self.username).delete()
-        connection.get_table('email').get_item(hash_key=self.email).delete()
-
+class TestTagResource(TestBase):
     def test_tag(self):
-        
         body = json.dumps({'tag': 'test tag', 'tags': ['foo', 'bar'], 'subscriptions': ['1234']})
-        request = Request('POST /tag/ HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n%s' % (self.session_id, body))
+        request = Request('POST /tag/ HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n%s' % (self.cookie, body))
         resource = TagResource(request)
         
+        self.assertEqual(resource.response.status, '201 Created')
+        
+        request = Request('GET /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.cookie, ))
         expected = '{"tags": {"foo": {"tags": {}, "subscriptions": {}}, "bar": {"tags": {}, "subscriptions": {}}}, "subscriptions": ["1234"]}'
-        
         self.assertEqual(resource.response.body, expected)
         
-        request = Request('GET /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.session_id, ))
-        resource = TagResource(request)
-        
-        self.assertEqual(resource.response.body, expected)
-        
-        request = Request('DELETE /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.session_id, ))
+        request = Request('DELETE /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.cookie, ))
         resource = TagResource(request)
         
         print resource.response.status
         
-        request = Request('GET /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.session_id, ))
+        request = Request('GET /tag/test%%20tag HTTP/1.0\r\nAccept: application/json\r\nCookie: %s\r\n\r\n' % (self.cookie, ))
         resource = TagResource(request)
         
         self.assertEqual(resource.response.status, '400 Bad Request')
---
+++ tests/testbase.py
@@ -0,0 +1,67 @@
+import unittest, json, sys, boto.dynamodb
+sys.path.insert(0, '..')
+from auth import generate_key, hash_password
+from request import Request
+from resource import timestamp
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        self.userid = generate_key()
+        self.username = 'thisisatestuser'
+        self.email = 'thisisatestemail@example.com'
+        self.password = 'thisisatestpassword'
+        self.sessionid = None
+        
+        connection =  boto.dynamodb.connect_to_region('eu-west-1')
+        
+        try:
+            username_item = connection.get_table('username').get_item(hash_key=self.username)
+            connection.get_table('user').get_item(hash_key=username_item['user']).delete()
+            username_item.delete()
+            # XXX scan these test user tags and delete them
+        except: pass
+        try: connection.get_table('email').get_item(hash_key=self.email).delete()
+        except: pass
+        
+        connection.get_table('user').new_item(
+            hash_key=self.userid,
+            attrs={
+                'username': self.username,
+                'email': self.email,
+                'password': hash_password(self.password),
+                }
+            ).put()
+        
+        connection.get_table('username').new_item(
+            hash_key=self.username,
+            attrs={
+                'user': self.userid,
+                }
+            ).put()
+        
+        connection.get_table('email').new_item(
+            hash_key=self.email,
+            attrs={
+                'user': self.userid,
+                }
+            ).put()
+        
+        self.sessionid = generate_key()
+        connection.get_table('session').new_item(
+            hash_key=self.sessionid,
+            attrs={
+                'user': self.userid,
+                'email': self.email,
+                'username': self.username,
+                'timestamp': timestamp()
+                }
+            ).put()        
+        self.cookie = 'session_id=' + self.sessionid + ';'
+        
+    def tearDown(self):
+        connection =  boto.dynamodb.connect_to_region('eu-west-1')
+        
+        connection.get_table('session').get_item(hash_key=self.sessionid).delete()
+        connection.get_table('user').get_item(hash_key=self.userid).delete()
+        connection.get_table('username').get_item(hash_key=self.username).delete()
+        connection.get_table('email').get_item(hash_key=self.email).delete()
