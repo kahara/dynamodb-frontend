5f9d4fbd329d475222e7ae4fabcb2a474b6c1812
Joni Kahara <joni.kahara@gmail.com>
2013-04-21 12:38:00 +0000

    limit number of list and dict items, and string lengths

--- striptags.py
+++ striptags.py
@@ -6,16 +6,24 @@ def escape(s):
 def striptags(obj):
     if type(obj) == DictType:
         r = {}
+        count = 0
         for key in obj:
+            if count >= 500:
+                break
+            count += 1
             r[key] = striptags(obj[key])
     
     elif type(obj) == ListType:
         r = []
+        count = 0
         for item in obj:
+            if count >= 500:
+                break
+            count += 1
             r.append(striptags(item))
             
     elif type(obj) == StringType or type(obj) == UnicodeType:
-        r = escape(obj)
+        r = escape(obj[0:50])
     
     else:
         r = obj
